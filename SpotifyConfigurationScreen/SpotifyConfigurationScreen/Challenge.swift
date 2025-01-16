fab9c30c0f465856bbee115e8761db00e8335879
Dwaraka-Poreddy <dwarakanatha.poreddy@gojek.com>
2025-01-16 08:31:47 +0530

    Make page scrollable in Portrait mode

--- SpotifyConfigurationScreen/SpotifyConfigurationScreen/Challenge.swift
+++ SpotifyConfigurationScreen/SpotifyConfigurationScreen/Challenge.swift
@@ -6,7 +6,7 @@
 //
 import UIKit
 
-class Challenge: UIViewController {
+class Challenge: UIViewController, UIScrollViewDelegate {
     
     let margin: CGFloat = 16
     
@@ -43,8 +43,15 @@ class Challenge: UIViewController {
      */
     
     func setupViews() {
+        lazy var scrollView: UIScrollView = {
+            let scroll = UIScrollView()
+            scroll.translatesAutoresizingMaskIntoConstraints = false
+            return scroll
+        }()
+        
         let stackView = makeStackView(withOrientation: .vertical)
         stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
         
         let offlineRow = RowView(title: "Offline", isOn: false)
         let offlineSublabel = makeSubLabel(withText: "When you go offline, you'll only be able to play the music and podcasts you've downloaded.")
@@ -64,11 +71,27 @@ class Challenge: UIViewController {
         stackView.addArrangedSubview(hideSongsRow)
         stackView.addArrangedSubview(normalizationRow)
         
-        view.addSubview(stackView)
+        view.addSubview(scrollView)
+        scrollView.addSubview(stackView)
         
-        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        // ScrollView constraints
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        // StackView constraints
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            stackView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor, constant: margin),
+            stackView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor, constant: -margin),
+            stackView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            stackView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor, constant: -2 * margin)
+        ])
+        
+        
         
         stackView.isLayoutMarginsRelativeArrangement = true
         stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: margin, leading: margin, bottom: margin, trailing: margin)
